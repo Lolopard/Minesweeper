8e3c981df09e543c2f409ae9642429473748450f
Lolopard <james2000@live.se>
2018-03-25 11:04:27 +0200

    oh shit waddup
    added the numbers around the mines and removed mines overlapping
    
    jetfuel cant melt steel beams

--- minesweeper_functions.py
+++ minesweeper_functions.py
@@ -9,11 +9,11 @@ def generate_board ( x_length , y_length , origin_x , origin_y , mine_ratio ):
     
     minefield = []
 
-    empty = " "
+    empty = 0
 
     mine = 'mine.png'
 
-    boarder = "shiz"
+    boarder = 10 #could any int, idk. maybe will be changed to 0 if memory is an issue
 
     mine_count = int( x_length * y_length * mine_ratio )
 
@@ -26,12 +26,29 @@ def generate_board ( x_length , y_length , origin_x , origin_y , mine_ratio ):
                 minefield[x].append(empty)
 
     for mines in range(mine_count):
-        mine_pos_x=random.randint(0,(x_length-1))
-        mine_pos_y=random.randint(0,(y_length-1))
-        while mine_pos_x == origin_x:
-            mine_pos_x=random.randint(0,(x_length-1))
-        while mine_pos_y == origin_y:
-            mine_pos_y=random.randint(0,(y_length-1))
-        minefield[mine_pos_x][mine_pos_y] = mine   #add numbers around
+        
+        reroll = True
+        while reroll:
+            reroll = False #Incase of a overlap
+            
+            mine_pos_x=random.randint(1,(x_length-2)) #initial roll
+            mine_pos_y=random.randint(1,(y_length-2))
+            
+            while mine_pos_x == origin_x:
+                mine_pos_x=random.randint(1,(x_length-2)) #reroll for the first click
+            while mine_pos_y == origin_y:
+                mine_pos_y=random.randint(1,(y_length-2))
+                
+            if minefield[mine_pos_x][mine_pos_y] == mine:
+                reroll = True #reroll for overlap
+                
+        for x_modifier in range(-1,2):
+            for y_modifier in range(-1,2):
+                if minefield[mine_pos_x + x_modifier][mine_pos_y + y_modifier] != mine:
+                    minefield[mine_pos_x + x_modifier][mine_pos_y + y_modifier] += 1
+        minefield[mine_pos_x][mine_pos_y] = mine
+
+generate_board(10,10,1,1,0.1)
+print(minefield)
 
             
